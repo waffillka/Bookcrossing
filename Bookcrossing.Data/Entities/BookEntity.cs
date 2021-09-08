4782d95ddf3edb30c7bbd61bb7fa1dafe5b7aa5a
George Sebik <waffilka@gmail.com>
2021-09-08 10:49:23 +0300

    Fix namespace for BookEntity class

--- Bookcrossing.Data/Entities/BookEntity.cs
+++ Bookcrossing.Data/Entities/BookEntity.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
-namespace Bookcrossing.Entities
+namespace Bookcrossing.Data.Entities
 {
     public class BookEntity
     {
