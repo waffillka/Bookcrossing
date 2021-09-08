56239bf80432fbe599bb66ac443cf28d037dce74
George Sebik <waffilka@gmail.com>
2021-09-08 17:07:15 +0300

    added interface Iidentifiable

---
+++ Bookcrossing.Data/Interfaces/IIdentifiable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookcrossing.Data.Interfaces
+{
+    public interface IIdentifiable<TIdentifierType>
+    {
+        TIdentifierType Id { get; set; }
+    }
+}
