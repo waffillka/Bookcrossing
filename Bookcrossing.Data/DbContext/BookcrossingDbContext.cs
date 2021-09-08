5d980dd55fb004087f48c14d9f9d2d3a30f696f9
George Sebik <waffilka@gmail.com>
2021-09-08 16:00:07 +0300

    Added relationships between models

--- Bookcrossing.Data/DbContext/BookcrossingDbContext.cs
+++ Bookcrossing.Data/DbContext/BookcrossingDbContext.cs
@@ -13,11 +13,19 @@ namespace Bookcrossing.Data.DbContext
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Book>()
+                .HasOne(c => c.Owner)
+                .WithMany(o => o.OwnerBook);
+
+            modelBuilder.Entity<Book>()
+                .HasOne(c => c.Recipient)
+                .WithMany(o => o.BookRecipient);
         }
 
         public DbSet<Book> Books { get; set; }
         public DbSet<Author> Authors { get; set; }
         public DbSet<Publisher> Publishers { get; set; }
+        public DbSet<User> Users { get; set; }
         public DbSet<HistoryOfIssuingBooks> HistoryOfIssuingBooks { get; set; }
     }
 }
