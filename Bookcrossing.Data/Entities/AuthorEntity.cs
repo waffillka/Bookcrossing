1e5962548d9379d63e5fe61d58cf948b7d142dfc
George Sebik <waffilka@gmail.com>
2021-09-08 10:32:36 +0300

    Create base entity models (Author, Book, Publisher, User)

---
+++ Bookcrossing.Data/Entities/AuthorEntity.cs
@@ -0,0 +1,16 @@
+﻿using Bookcrossing.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Bookcrossing.Data.Entities
+{
+    public class AuthorEntity
+    {
+        [Required]
+        public Guid Id;
+        [Required]
+        public string Name;
+        public ICollection<BookEntity>? Books;
+    }
+}
---
+++ Bookcrossing.Data/Entities/PublisherEntity.cs
@@ -0,0 +1,17 @@
+﻿using Bookcrossing.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Bookcrossing.Data.Entities
+{
+    public class PublisherEntity
+    {
+        [Required]
+        public Guid Id;
+        [Required]
+        public string Name;
+
+        public ICollection<BookEntity>? Books;
+    }
+}
