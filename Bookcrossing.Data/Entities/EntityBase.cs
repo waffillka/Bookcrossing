8706406b3e61d17d2bb5da0eb90eadc859772011
George Sebik <waffilka@gmail.com>
2021-09-08 17:08:03 +0300

    Create EntityBase

---
+++ Bookcrossing.Data/Entities/EntityBase.cs
@@ -0,0 +1,15 @@
+﻿using Bookcrossing.Data.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookcrossing.Data.Entities
+{
+    public class EntityBase : IIdentifiable<Guid>, ISoftDeleteable
+    {
+        public Guid Id { get; set; }
+        public bool isDeleted { get; set; }
+    }
+}
