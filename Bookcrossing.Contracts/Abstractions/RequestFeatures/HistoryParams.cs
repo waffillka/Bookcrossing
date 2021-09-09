f40500848867ecf934f4b1d558fa4f74f4eedb16
George Sebik <waffilka@gmail.com>
2021-09-09 17:28:31 +0300

    Create class for history params

---
+++ Bookcrossing.Contracts/Abstractions/RequestFeatures/HistoryParams.cs
@@ -0,0 +1,16 @@
+﻿using Bookcrossing.Contracts.Enumerations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookcrossing.Contracts.Abstractions.RequestFeatures
+{
+    public class HistoryParams : RequestFeatures
+    {
+        public string OrderBy { get; set; } = "abc"; // abc/cba
+        public DateTime From { get; set; } = default;
+        public DateTime To { get; set; } = default;
+    }
+}
