b31d3bfd76f73dec8e542da2a9f5f4203d8fa75d
George Sebik <waffilka@gmail.com>
2021-09-09 16:50:17 +0300

    implement interface Publisher repository

---
+++ Bookcrossing.Data/Repositories/Interface/IPublisherRepository.cs
@@ -0,0 +1,13 @@
+﻿using Bookcrossing.Data.Entities;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookcrossing.Data.Repositories.Interface
+{
+    public interface IPublisherRepository : IRepositoryBase<Publisher>
+    {
+        Task<IQueryable<Publisher>> GetByName(string name);
+        Task<Publisher> GetById(Guid id);
+    }
+}
---
+++ Bookcrossing.Data/Repositories/PublisherRepository.cs
@@ -0,0 +1,30 @@
+﻿using Bookcrossing.Data.DbContext;
+using Bookcrossing.Data.Entities;
+using Bookcrossing.Data.Repositories.Interface;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookcrossing.Data.Repositories
+{
+    public class PublisherRepository : RepositoryBase<Publisher>, IPublisherRepository
+    {
+        public PublisherRepository(BookcrossingDbContext dbContext)
+            : base(dbContext)
+        {
+
+        }
+
+        public async Task<Publisher> GetById(Guid id)
+        {
+            var entity = _dbContext.Publishers.FirstOrDefault(x => x.Id == id && !x.IsDeleted);
+            return entity;
+        }
+
+        public async Task<IQueryable<Publisher>> GetByName(string name)
+        {
+            var authors = _dbContext.Publishers.Where(x => x.Name.Contains(name) && !x.IsDeleted);
+            return authors;
+        }
+    }
+}
