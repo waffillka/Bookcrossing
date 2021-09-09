43793c60a2cd0d8f83bfc084b41759909ad62f2c
George Sebik <waffilka@gmail.com>
2021-09-09 16:49:22 +0300

    Implement interface Author repository

---
+++ Bookcrossing.Data/Repositories/AuthorRepository.cs
@@ -0,0 +1,31 @@
+﻿using Bookcrossing.Data.DbContext;
+using Bookcrossing.Data.Entities;
+using Bookcrossing.Data.Repositories.Interface;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Bookcrossing.Data.Repositories
+{
+    public class AuthorRepository : RepositoryBase<Author>, IAuthorRepository
+    {
+        public AuthorRepository(BookcrossingDbContext dbContext)
+            :base(dbContext)
+        {
+            
+        }
+
+        public async Task<Author> GetById(Guid id)
+        {
+            var entity = _dbContext.Authors.FirstOrDefault(x => x.Id == id && !x.IsDeleted);
+            return entity;
+        }
+
+        public async Task<IQueryable<Author>> GetByName(string name)
+        {
+            var authors = _dbContext.Authors.Where(x => x.Name.Contains(name) && !x.IsDeleted);
+            return authors;
+        }
+    }
+}
---
+++ Bookcrossing.Data/Repositories/Interface/IAuthorRepository.cs
@@ -0,0 +1,13 @@
+﻿using Bookcrossing.Data.Entities;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookcrossing.Data.Repositories.Interface
+{
+    public interface IAuthorRepository : IRepositoryBase<Author>
+    {
+        Task<IQueryable<Author>> GetByName(string name);
+        Task<Author> GetById(Guid id);
+    }
+}
