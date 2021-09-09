19c1c1140462de0dd116f5d3b600556018306bb4
George Sebik <waffilka@gmail.com>
2021-09-09 16:51:47 +0300

    Create class and interface HistoryOfIssuingBooksRepository

---
+++ Bookcrossing.Data/Repositories/HistoryOfIssuingBooksRepository.cs
@@ -0,0 +1,20 @@
+﻿using Bookcrossing.Data.DbContext;
+using Bookcrossing.Data.Entities;
+using Bookcrossing.Data.Repositories.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookcrossing.Data.Repositories
+{
+    public class HistoryOfIssuingBooksRepository : RepositoryBase<HistoryOfIssuingBooks>, IHistoryOfIssuingBooksRepository
+    {
+        public HistoryOfIssuingBooksRepository(BookcrossingDbContext dbContext)
+           : base(dbContext)
+        {
+
+        }
+    }
+}
---
+++ Bookcrossing.Data/Repositories/Interface/IHistoryOfIssuingBooksRepository.cs
@@ -0,0 +1,12 @@
+﻿using Bookcrossing.Data.Entities;
+using System;
+using System.Threading.Tasks;
+
+namespace Bookcrossing.Data.Repositories.Interface
+{
+    public interface IHistoryOfIssuingBooksRepository : IRepositoryBase<HistoryOfIssuingBooks>
+    {
+        Task<HistoryOfIssuingBooks> GetByBook(Guid id);
+
+    }
+}
