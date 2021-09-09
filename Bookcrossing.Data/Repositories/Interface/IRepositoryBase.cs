176faa08437d8ebfedaf7edc11f724324195794a
George Sebik <waffilka@gmail.com>
2021-09-09 11:36:23 +0300

    Create interface for repository base

---
+++ Bookcrossing.Data/Repositories/Interface/IRepositoryBase.cs
@@ -0,0 +1,16 @@
+﻿using Bookcrossing.Data.Interfaces;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Bookcrossing.Data.Repositories.Interface
+{
+    public interface IRepositoryBase<TEntity>
+        where TEntity : IIdentifiable<Guid>, ISoftDeleteable
+    {
+        Task<TEntity> GetAsync(Guid id, CancellationToken ct = default);
+        Task<TEntity> InsertAsync(TEntity newEntity, CancellationToken ct = default);
+        Task<TEntity> UpdateAsync(TEntity entityToUpdate, CancellationToken ct = default);
+        Task<TEntity> DeleteAsync(TEntity entityToRemove, CancellationToken ct = default);
+    }
+}
