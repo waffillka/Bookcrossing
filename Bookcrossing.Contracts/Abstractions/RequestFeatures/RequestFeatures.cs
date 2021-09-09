a376317c2e3cba3b02c6442fefc3c1bc003d52c6
George Sebik <waffilka@gmail.com>
2021-09-09 11:49:36 +0300

    Create abstract class for pagination.

---
+++ Bookcrossing.Contracts/Abstractions/RequestFeatures/RequestFeatures.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookcrossing.Contracts.Abstractions.RequestFeatures
+{
+    public abstract class RequestFeatures
+    {
+        private const int _maxPageSize = 100;
+        public int PageNumber { get; set; } = 1;
+        private int _pageSize = 10;
+        public int PageSize
+        {
+            get
+            {
+                return _pageSize;
+            }
+            set
+            {
+                _pageSize = (value > _maxPageSize) ? _maxPageSize : value;
+            }
+        }
+    }
+}
