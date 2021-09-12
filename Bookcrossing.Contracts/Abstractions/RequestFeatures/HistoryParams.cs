3405124e749112d8cb38f85d179529f2b282736f
George Sebik <waffilka@gmail.com>
2021-09-12 22:41:34 +0300

    added params

--- Bookcrossing.Contracts/Abstractions/RequestFeatures/HistoryParams.cs
+++ Bookcrossing.Contracts/Abstractions/RequestFeatures/HistoryParams.cs
@@ -1,9 +1,4 @@
-﻿using Bookcrossing.Contracts.Enumerations;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;
 
 namespace Bookcrossing.Contracts.Abstractions.RequestFeatures
 {
