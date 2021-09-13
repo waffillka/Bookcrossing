339b5b56bd2d2743f650acc62944e6d55d196f1f
George Sebik <waffilka@gmail.com>
2021-09-13 19:52:13 +0300

    Creaate unit of work (repository manager)

---
+++ Bookcrossing.Data/Repositories/Interface/IRepositoryManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookcrossing.Data.Repositories.Interface
+{
+    public interface IRepositoryManager
+    {
+        IAuthorRepository Authors { get; }
+        IBookRepository Books { get; }
+        IHistoryOfIssuingBooksRepository History { get; }
+        IPublisherRepository Publisher { get; }
+        Task SaveAsync();
+    }
+}
