47c5eabaf6aeb21614a8de2d1c65ca75ba4a78ea
George Sebik <waffilka@gmail.com>
2021-09-13 13:37:36 +0300

    request parametrs for author and publisher

---
+++ Bookcrossing.Contracts/Abstractions/RequestFeatures/AuthorPublisherParams.cs
@@ -0,0 +1,8 @@
+﻿namespace Bookcrossing.Contracts.Abstractions.RequestFeatures
+{
+    public class AuthorPublisherParams
+    {
+        public string OrderyBy { get; set; } = "abc";
+        public string MatchString { get; set; }
+    }
+}
