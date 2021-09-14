a8b04e2e3b97bf27bee6bd80e04ffaed307ffd18
George Sebik <waffilka@gmail.com>
2021-09-14 14:53:10 +0300

    Fix method GetAsync

--- Bookcrossing.Data/Repositories/BookRepository.cs
+++ Bookcrossing.Data/Repositories/BookRepository.cs
@@ -1,11 +1,10 @@
 ﻿using Bookcrossing.Contracts.Abstractions.RequestFeatures;
+using Bookcrossing.Contracts.Enumeration;
 using Bookcrossing.Data.DbContext;
 using Bookcrossing.Data.Entities;
 using Bookcrossing.Data.Repositories.Interface;
-using Microsoft.EntityFrameworkCore;
 using System;
 using System.Linq;
-using System.Linq.Expressions;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -21,41 +20,38 @@ namespace Bookcrossing.Data.Repositories
 
         public async Task<IQueryable<Book>> GetAsync(BookParams parameters, CancellationToken ct = default)
         {
+            if (parameters == null)
+            {
+                throw new ArgumentNullException(nameof(parameters));
+            }
+
             IQueryable<Book> entities = _dbContext.Books;
 
-            parameters ??= new BookParams();
-
-            if (string.IsNullOrEmpty(parameters.MatchString))
+            if (!string.IsNullOrWhiteSpace(parameters.SearchStringh))
             {
-                entities = entities.Where(x => x.Name.Contains(parameters.MatchString));
+                entities = entities.Where(x => x.Name.Contains(parameters.SearchStringh)
+                                                || x.Description.Contains(parameters.SearchStringh)
+                                                || x.Authors.Any(c => c.Name.Contains(parameters.SearchStringh)) // что-то похожее на 48 строке 
+                                                || x.ISBIN.Contains(parameters.SearchStringh)
+                                                || x.Publisher.Name.Contains(parameters.SearchStringh));
             }
 
-            if (parameters.OrderyBy == "abc")
-            {
-                entities.OrderBy(x => x.Name);
-            }
-            else
-            {
-                entities.OrderByDescending(x => x.Name);
-            }
-
-            if (parameters.IsFree)
+            if (parameters.IsFree == true)
             {
                 entities = entities.Where(x => x.RecipientId != null);
-            }
-            else
-            {
-                entities = entities.Where(x => x.RecipientId == null);
             }
 
             if (parameters.Authors != null)
             {
                 entities = entities.Where(book => book.Authors.Any(x => parameters.Authors.Contains(x.Name)));
             }
+
             if (parameters.Publishers != null)
             {
                 entities = entities.Where(book => parameters.Publishers.Contains(book.Publisher.Name));
             }
+
+            entities = parameters.OrderyBy == Ordery.Asc ? entities.OrderBy(x => x.Name) : entities.OrderByDescending(x => x.Name);
 
             return entities.Skip((parameters.PageNumber - 1) * parameters.PageSize)
                 .Take(parameters.PageSize);
--- Bookcrossing.Data/Repositories/Interface/IBookRepository.cs
+++ Bookcrossing.Data/Repositories/Interface/IBookRepository.cs
@@ -1,6 +1,5 @@
 ﻿using Bookcrossing.Contracts.Abstractions.RequestFeatures;
 using Bookcrossing.Data.Entities;
-using System;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
