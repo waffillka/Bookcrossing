034109ae9a235edd4f91b3085b0c271435b4c7b8
George Sebik <waffilka@gmail.com>
2021-09-14 16:58:12 +0300

    refactoring entities

--- Bookcrossing.Data/Entities/Book.cs
+++ Bookcrossing.Data/Entities/Book.cs
@@ -19,12 +19,12 @@ namespace Bookcrossing.Data.Entities
         [MaxLength(17)]
         public string ISBIN { get; set; }
         public Guid PublisherId { get; set; }
-        public Guid OwnerId { get; set; }
-        public Guid? RecipientId { get; set; }
-
-        public virtual User Owner { get; set; }
         public virtual Publisher Publisher { get; set; }
-        public virtual User Recipient { get; set; }
+        public Guid OwnerId { get; set; }
+        public virtual User Owner { get; set; }
+        public Guid? RecipientId { get; set; }
+        public virtual User Recipient { get; set; } 
+        
         public virtual ICollection<Author> Authors { get; set; }
 
         [NotMapped]
--- Bookcrossing.Data/Entities/HistoryOfIssuingBooks.cs
+++ Bookcrossing.Data/Entities/HistoryOfIssuingBooks.cs
@@ -5,11 +5,10 @@ namespace Bookcrossing.Data.Entities
     public class HistoryOfIssuingBooks : EntityBase
     {
         public Guid UserId { get; set; }
-        public Guid BookId { get; set; }
-        public DateTime DateOfReceiving { get; set; }
-        public DateTime DateOfDelivery { get; set; }
-
-        public virtual Book Book { get; set; }
         public virtual User User { get; set; }
+        public Guid BookId { get; set; }
+        public virtual Book Book { get; set; }
+        public DateTime DateOfReceiving { get; set; }
+        public DateTime DateOfDelivery { get; set; } 
     }
 }
