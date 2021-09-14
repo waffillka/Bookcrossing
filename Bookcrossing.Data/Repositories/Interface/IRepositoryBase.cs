aac8668e45a774cb9ecd024fe5c02bdd33a4341e
George Sebik <waffilka@gmail.com>
2021-09-14 16:21:24 +0300

    fixed syntax error

--- Bookcrossing.Data/Repositories/Interface/IRepositoryBase.cs
+++ Bookcrossing.Data/Repositories/Interface/IRepositoryBase.cs
@@ -15,7 +15,7 @@ namespace Bookcrossing.Data.Repositories.Interface
         Task<IQueryable<TEntity>> GetAsync(RequestFeatures pagination, CancellationToken ct = default);
         Task<TEntity> InsertAsync(TEntity newEntity, CancellationToken ct = default);
         Task InsertAsync(TEntity[] newEntity, CancellationToken ct = default);
-        Task<TEntity> UpdateAsync(TEntity entityToUpdate, CancellationToken ct = default)
+        Task<TEntity> UpdateAsync(TEntity entityToUpdate, CancellationToken ct = default);
         Task Delete(TEntity entityToRemove, bool hard, CancellationToken ct = default);
         Task Delete(TEntity[] entityToRemove, bool hard, CancellationToken ct = default);
         Task<IQueryable<TEntity>> GetByCondition(Expression<Func<TEntity, bool>> expression, RequestFeatures parameters, CancellationToken ct = default);
