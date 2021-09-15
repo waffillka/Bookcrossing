c16a5f349142252ab0797aca8ba2b1182c815ded
George Sebik <waffilka@gmail.com>
2021-09-15 13:45:18 +0300

    Create config file for mapper

---
+++ Bookcrossing.Application/Mapping/MappingConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Bookcrossing.Contracts.DataTransferObjects;
+using Bookcrossing.Data.Entities;
+
+namespace Bookcrossing.Application.Mapping
+{
+    public class MappingConfiguration : Profile
+    {
+        public MappingConfiguration()
+        {
+            CreateMap<Author, AuthorDto>().ReverseMap();
+            CreateMap<Book, BookDto>().ReverseMap();
+            CreateMap<HistoryOfIssuingBooks, HistoryDto>();
+            CreateMap<Publisher, PublisherDto>().ReverseMap();
+            CreateMap<User, UserDto>();
+           
+
+        }
+    }
+}
