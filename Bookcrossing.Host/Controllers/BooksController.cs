577f12ea925f46c7827d4a205c994aae11ede617
George Sebik <waffilka@gmail.com>
2021-09-15 14:07:01 +0300

    clean dependencies

--- Bookcrossing.Host/Controllers/BooksController.cs
+++ Bookcrossing.Host/Controllers/BooksController.cs
@@ -1,11 +1,6 @@
 ﻿using Bookcrossing.Application.Commands.Book;
-using Bookcrossing.Contracts.Abstractions.RequestFeatures;
 using MediatR;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace Bookcrossing.Host.Controllers
