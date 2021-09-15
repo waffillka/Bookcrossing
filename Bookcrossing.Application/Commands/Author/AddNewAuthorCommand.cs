c837a62a22960416cb37f369e72fc83a9e383eaf
George Sebik <waffilka@gmail.com>
2021-09-15 17:37:58 +0300

    added add and delete command

---
+++ Bookcrossing.Application/Commands/Author/AddNewAuthorCommand.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using Bookcrossing.Contracts.DataTransferObjects.Creation;
+using Bookcrossing.Contracts.DataTransferObjects.Deteils;
+using Bookcrossing.Data.Repositories.Interface;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Bookcrossing.Application.Commands.Author
+{
+    public class AddNewAuthorCommand : IRequest<AuthorDeteilsDto>
+    {
+        public AddNewAuthorCommand(AuthorCreationDto athor)
+        {
+            Athor = athor;
+        }
+
+        public AuthorCreationDto Athor { get; }
+    }
+
+    public class AddNewAuthorCommandHandler : IRequestHandler<AddNewAuthorCommand, AuthorDeteilsDto>
+    {
+        private readonly IMapper _mapper;
+        private readonly IAuthorRepository _authorRepository;
+        public AddNewAuthorCommandHandler(IMapper mapper, IAuthorRepository authorRepository)
+        {
+            _mapper = mapper;
+            _authorRepository = authorRepository;
+        }
+
+        public async Task<AuthorDeteilsDto> Handle(AddNewAuthorCommand request, CancellationToken ct)
+        {
+            var entity = _mapper.Map<Data.Entities.Author>(request.Athor);
+            entity = await _authorRepository.InsertAsync(entity, ct);
+            var result = _mapper.Map<AuthorDeteilsDto>(entity);
+
+            return result;
+        }
+    }
+}
---
+++ Bookcrossing.Application/Commands/Author/DeleteAuthorNoHardCommand.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using Bookcrossing.Data.Repositories.Interface;
+using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Bookcrossing.Application.Commands.Author
+{
+    public class DeleteAuthorNoHardCommand : IRequest
+    {
+        public DeleteAuthorNoHardCommand(Guid idAuthor)
+        {
+            IdAuthor = idAuthor;
+        }
+
+        public Guid IdAuthor { get; }
+    }
+
+    public class DeleteAuthorCommandHandler : IRequestHandler<DeleteAuthorNoHardCommand>
+    {
+        private readonly IAuthorRepository _authorRepository;
+        private readonly IMapper _mapper;
+
+        public DeleteAuthorCommandHandler(IAuthorRepository authorRepository, IMapper mapper)
+        {
+            _authorRepository = authorRepository;
+            _mapper = mapper;
+        }
+        public async Task<Unit> Handle(DeleteAuthorNoHardCommand request, CancellationToken ct)
+        {
+            var entityToRemove = await _authorRepository.GetOneByCondition(x => x.Id == request.IdAuthor, ct);
+            _authorRepository.Delete(entityToRemove, ct);
+
+            return Unit.Value;
+        }      
+    }
+}
