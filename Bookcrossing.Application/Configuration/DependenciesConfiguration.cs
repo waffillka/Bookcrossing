8aa59d0b80efba984fb6165b849ae105ffab9169
George Sebik <waffilka@gmail.com>
2021-09-15 12:24:53 +0300

    fix query GerBooksWithParams

---
+++ Bookcrossing.Application/Configuration/DependenciesConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Bookcrossing.Application.Configuration
+{
+    public static class DependenciesConfiguration
+    {
+        public static void BookcrossingService(this IServiceCollection services)
+        {
+            services.RegisterMediator();
+        }
+
+        private static void RegisterMediator(this IServiceCollection services)
+        {
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+        }
+    }
+}
---
+++ Bookcrossing.Application/Queries/Book/GetBooksWithParametersQuery.cs
@@ -0,0 +1,36 @@
+﻿using Bookcrossing.Contracts.Abstractions.RequestFeatures;
+using Bookcrossing.Data.Repositories.Interface;
+using MediatR;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Bookcrossing.Application.Commands.Book
+{
+    public class GetBooksWithParametersQuery : IRequest<IReadOnlyCollection<Data.Entities.Book>>
+    {
+        public GetBooksWithParametersQuery(BookParams parameters)
+        {
+            Parameters = parameters;
+        }
+
+        public BookParams Parameters { get; }
+    }
+
+    public class GetBooksWithParametersQueryHandler : IRequestHandler<GetBooksWithParametersQuery, IReadOnlyCollection<Data.Entities.Book>>
+    {
+        private readonly IBookRepository _bookRepository;
+
+        public GetBooksWithParametersQueryHandler(IBookRepository bookRepository)
+        {
+            _bookRepository = bookRepository;
+        }
+        public async Task<IReadOnlyCollection<Data.Entities.Book>> Handle(GetBooksWithParametersQuery request, CancellationToken cancellationToken)
+        {
+            var books = await _bookRepository.GetAsync(request.Parameters);
+
+            return books;
+        }
+    }
+}
