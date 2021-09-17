fbfe93044e39b79cedbeb3474968987cfbcf0044
George Sebik <waffilka@gmail.com>
2021-09-17 18:00:03 +0300

    Create head handle

---
+++ Bookcrossing.Application/Handler/LoggerRequestHandler.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Bookcrossing.Application.Handler
+{
+    public abstract class LoggerRequestHandler<TRequest, TResponse> : IRequestHandler<TRequest, TResponse>
+        where TRequest : IRequest<TResponse>
+    {
+
+        public Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken)
+        {
+            // log
+
+            return HandleInternalAsync(request, cancellationToken);
+        }
+
+        public abstract Task<TResponse> HandleInternalAsync(TRequest request, CancellationToken cancellationToken);
+    }
+}
