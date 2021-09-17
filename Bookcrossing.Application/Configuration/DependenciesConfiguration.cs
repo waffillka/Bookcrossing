81b8e59a163573807fc530ec5c07d63623f93721
George Sebik <waffilka@gmail.com>
2021-09-17 14:09:42 +0300

    Register Fluent Validation

--- Bookcrossing.Application/Configuration/DependenciesConfiguration.cs
+++ Bookcrossing.Application/Configuration/DependenciesConfiguration.cs
@@ -1,4 +1,6 @@
 ﻿using Bookcrossing.Application.Mapping;
+using Bookcrossing.Application.Validators.Pipeline;
+using FluentValidation;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
@@ -7,15 +9,35 @@ namespace Bookcrossing.Application.Configuration
 {
     public static class DependenciesConfiguration
     {
-        public static void BookcrossingService(this IServiceCollection services)
+        public static void BookcrossingApplication(this IServiceCollection services)
         {
             services.RegisterMediator();
             services.RegisterMapping();
+            services.RegisterFluentValidation();
         }
 
         private static void RegisterMediator(this IServiceCollection services)
         {
             services.AddMediatR(Assembly.GetExecutingAssembly());
+        }
+
+        private static void RegisterFluentValidation(this IServiceCollection services)
+        {
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            AssemblyScanner.FindValidatorsInAssembly(Assembly.GetExecutingAssembly())
+                .ForEach(item => services.AddScoped(item.InterfaceType, item.ValidatorType));
+
+            services.AddScoped(typeof(IPipelineBehavior<,>), typeof(PipelineValidationBehavior<,>));
+
+            services.Scan(
+                x => {
+                    var assemby = Assembly.GetExecutingAssembly();
+
+                    x.FromAssemblies(assemby)
+                        .AddClasses(classes => classes.AssignableTo(typeof(IValidator<>)))
+                        .AsImplementedInterfaces()
+                        .WithScopedLifetime();
+                });
         }
 
         private static void RegisterMapping(this IServiceCollection services)
---
+++ Bookcrossing.Application/Validators/Pipeline/PipelineValidationBehavior.cs
@@ -0,0 +1,40 @@
+﻿using FluentValidation;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Bookcrossing.Application.Validators.Pipeline
+{
+    public class PipelineValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
+    {
+        private readonly IEnumerable<IValidator> _validators;
+
+        public PipelineValidationBehavior(IEnumerable<IValidator<TRequest>> validators)
+        {
+            _validators = validators;
+        }
+
+        public Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            var validationFailures = _validators
+                .Select(validator => validator.Validate(new ValidationContext<TRequest>(request)))
+                .SelectMany(validationResult => validationResult.Errors)
+                .Where(validationFailure => validationFailure != null)
+                .ToList();
+
+            if (validationFailures.Any())
+            {
+                var error = string.Join("\r\n", validationFailures);
+
+                //TODO 1 Change exception
+                throw new ArgumentException(error);
+            }
+
+            return next();
+        }
+    }
+}
