e7225e5923089e80981a80f7f1abca81f30b69c9
George Sebik <waffilka@gmail.com>
2021-09-17 14:12:23 +0300

    Exception model for Exception Handler

---
+++ Bookcrossing.Contracts/Exceptions/ModelExceptions/ErrorDetails.cs
@@ -0,0 +1,11 @@
+﻿using Newtonsoft.Json;
+
+namespace Bookcrossing.Contracts.Exceptions.ModelExceptions
+{
+    public class ErrorDetails
+    {
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+        public override string ToString() => JsonConvert.SerializeObject(this);
+    }
+}
