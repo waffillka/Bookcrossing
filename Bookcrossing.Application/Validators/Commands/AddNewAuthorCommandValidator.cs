a9fe671c47d9f52ed1bada4488a8e947b7918158
George Sebik <waffilka@gmail.com>
2021-09-17 14:46:32 +0300

    Added validators

---
+++ Bookcrossing.Application/Validators/Commands/AddNewAuthorCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using Bookcrossing.Application.Commands.Author;
+using FluentValidation;
+
+namespace Bookcrossing.Application.Validators.Commands
+{
+    public class AddNewAuthorCommandValidator : AbstractValidator<AddNewAuthorCommand>
+    {
+        public AddNewAuthorCommandValidator()
+        {
+            RuleFor(x => x.Author.Name).NotEmpty();
+            RuleFor(x => x.Author.Birthday).LessThan(System.DateTime.Now);
+        }
+    }
+}
---
+++ Bookcrossing.Application/Validators/Commands/AddNewPublisherCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using Bookcrossing.Application.Commands.Publisher;
+using FluentValidation;
+
+namespace Bookcrossing.Application.Validators.Commands
+{
+    public class AddNewPublisherCommandValidator : AbstractValidator<AddNewPublisherCommand>
+    {
+        public AddNewPublisherCommandValidator()
+        {
+            RuleFor(x => x.Publisher.Name).NotEmpty();
+        }
+    }
+}
