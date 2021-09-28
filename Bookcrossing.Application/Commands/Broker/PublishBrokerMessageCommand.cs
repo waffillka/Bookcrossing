64ead07f2978cbf6340b6d9e64b662724bd815c9
George Sebik <waffilka@gmail.com>
2021-09-28 18:25:39 +0300

    fix code style

--- Bookcrossing.Application/Commands/Broker/PublishBrokerMessageCommand.cs
+++ Bookcrossing.Application/Commands/Broker/PublishBrokerMessageCommand.cs
@@ -3,10 +3,6 @@ using Bookcrossing.Application.Logger;
 using Bookcrossing.Contracts.DataTransferObjects.Broker;
 using MassTransit;
 using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
