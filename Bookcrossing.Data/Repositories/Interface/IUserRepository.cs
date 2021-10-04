cb1308f210e606ad2681389a46d851a1c52cceba
George Sebik <waffilka@gmail.com>
2021-10-04 16:38:50 +0300

    added user repository

---
+++ Bookcrossing.Data/Repositories/Interface/IUserRepository.cs
@@ -0,0 +1,8 @@
+﻿using Bookcrossing.Data.Entities;
+
+namespace Bookcrossing.Data.Repositories.Interface
+{
+    public interface IUserRepository : IRepositoryBase<User>
+    {
+    }
+}
---
+++ Bookcrossing.Data/Repositories/UserRepository.cs
@@ -0,0 +1,15 @@
+﻿using Bookcrossing.Data.DbContext;
+using Bookcrossing.Data.Entities;
+using Bookcrossing.Data.Repositories.Interface;
+
+namespace Bookcrossing.Data.Repositories
+{
+    public class UserRepository : RepositoryBase<User>, IUserRepository
+    {
+        public UserRepository(BookcrossingDbContext dbContext)
+           : base(dbContext)
+        {
+
+        }
+    }
+}
