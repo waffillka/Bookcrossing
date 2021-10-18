e9ee873338694bc95fe3bc55543c409f8818ad7e
George Sebik <waffilka@gmail.com>
2021-10-18 17:06:26 +0300

    added migration

---
+++ Bookcrossing.Data/Migrations/20211018113242_ChangeEntities.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Bookcrossing.Data.Migrations
+{
+    public partial class ChangeEntities : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "UserAuthId",
+                table: "Users",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserAuthId",
+                table: "Users");
+        }
+    }
+}
