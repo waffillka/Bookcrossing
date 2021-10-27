e3f07f16c24cf4b2b121ed2e7e94cc80126d4a34
George Sebik <waffilka@gmail.com>
2021-10-27 20:54:51 +0300

    added user's query

--- Bookcrossing.Application/Queries/User/GetUserByAuthId.cs
+++ Bookcrossing.Application/Queries/User/GetUserByAuthId.cs
@@ -1,6 +1,42 @@
-﻿namespace Bookcrossing.Application.Queries.User
+﻿using AutoMapper;
+using Bookcrossing.Application.Handler;
+using Bookcrossing.Application.Logger;
+using Bookcrossing.Contracts.DataTransferObjects.Deteils;
+using Bookcrossing.Data.Repositories.Interface;
+using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Bookcrossing.Application.Queries.User
 {
-    public class GetUserByAuthId
+    public class GetUserByAuthId : IRequest<UserDeteilsDto>
     {
+        public GetUserByAuthId(Guid id)
+        {
+            AuthId = id;
+        }
+
+        public Guid AuthId { get; set; }
+    }
+
+    public class GetUserByAuthIdHandler : BaseRequestHandler<GetUserByAuthId, UserDeteilsDto>
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
+
+        public GetUserByAuthIdHandler(IUserRepository userRepository, IMapper mapper, ILoggerManager logger)
+            : base(logger)
+        {
+            _userRepository = userRepository;
+            _mapper = mapper;
+        }
+
+        public override async Task<UserDeteilsDto> HandleInternalAsync(GetUserByAuthId request, CancellationToken ct)
+        {
+            var entity = await _userRepository.GetByIdAsync(request.AuthId, ct);
+
+            return _mapper.Map<UserDeteilsDto>(entity);
+        }
     }
 }
