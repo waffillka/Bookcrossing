7c630e3ec89cc297876ddfaa62639550c88fd311
George Sebik <waffilka@gmail.com>
2021-10-27 18:17:11 +0300

    added middleware

--- Bookcrossing.Host/Middleware/ConfigureUserContextMiddleware.cs
+++ Bookcrossing.Host/Middleware/ConfigureUserContextMiddleware.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNetCore.Http;
 using System;
 using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace Bookcrossing.Host.Middleware
@@ -20,15 +19,12 @@ namespace Bookcrossing.Host.Middleware
 
         public async Task InvokeAsync(HttpContext httpContext, IClientUserContext clientUserContext)
         {
-            CancellationToken cancellationToken = httpContext?.RequestAborted ?? CancellationToken.None;
 
             if (AuthorizationHelper.TryGetAuthorizationTokenFromHttpHeaders(httpContext, out string token))
             {
                 var handler = new JwtSecurityTokenHandler();
                 var jsonToken = handler.ReadToken(token);
-
                 var tokenS = jsonToken as JwtSecurityToken;
-
 
                 clientUserContext.Name = tokenS.Claims?.FirstOrDefault(x => x.Type.Equals("name", StringComparison.OrdinalIgnoreCase))?.Value;
                 clientUserContext.Email = tokenS.Claims?.FirstOrDefault(x => x.Type.Equals("email", StringComparison.OrdinalIgnoreCase))?.Value;
