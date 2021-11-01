d574561212e0448338e44a84893025f0a95b0a4e
George Sebik <waffilka@gmail.com>
2021-11-01 16:31:41 +0300

    added base command for Recipient

---
+++ Bookcrossing.Application/Commands/User/AddRecipient.cs
@@ -0,0 +1,34 @@
+﻿using Bookcrossing.Application.Handler;
+using Bookcrossing.Application.Logger;
+using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Bookcrossing.Application.Commands.User
+{
+    public class AddRecipient : IRequest<bool>
+    {
+        public AddRecipient(Guid bookId, Guid userId)
+        {
+            BookId = bookId;
+            UserId = userId;
+        }
+
+        public Guid BookId { get; }
+        public Guid UserId { get; }
+    }
+
+    public class AddRecipientHandler : BaseRequestHandler<AddRecipient, bool>
+    {
+        public AddRecipientHandler(ILoggerManager logger)
+            : base(logger)
+        {
+        }
+
+        public override async Task<bool> HandleInternalAsync(AddRecipient request, CancellationToken ct)
+        {
+            return false;
+        }
+    }
+}
---
+++ Bookcrossing.Application/Commands/User/RemoveRecipient.cs
@@ -0,0 +1,34 @@
+﻿using Bookcrossing.Application.Handler;
+using Bookcrossing.Application.Logger;
+using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Bookcrossing.Application.Commands.User
+{
+    public class RemoveRecipient : IRequest<bool>
+    {
+        public RemoveRecipient(Guid bookId, Guid userId)
+        {
+            BookId = bookId;
+            UserId = userId;
+        }
+
+        public Guid BookId { get; }
+        public Guid UserId { get; }
+    }
+
+    public class RemoveRecipientHandler : BaseRequestHandler<RemoveRecipient, bool>
+    {
+        public RemoveRecipientHandler(ILoggerManager logger)
+            : base(logger)
+        {
+        }
+
+        public override async Task<bool> HandleInternalAsync(RemoveRecipient request, CancellationToken ct)
+        {
+            return false;
+        }
+    }
+}
